d586abeb3904f1baceff38b564a81bd503f525db
vaibhav-OP <vaibhavyash28@gmail.com>
2022-12-27 20:59:49 +0530

    sign-in page css

--- client/app/auth/sign-in/page.jsx
+++ client/app/auth/sign-in/page.jsx
@@ -42,12 +42,31 @@ export default function SignIn() {
     }
 
     return (
-        <div>
-            <form onSubmit={signIn}>
-                <input type="email" placeholder="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/><br />
-                <input type="password" placeholder="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/><br />
-                <input type="submit" value="submit"/>
-            </form>
+        <div className=''>
+            <div className='flex justify-center items-center'>
+                <div className='flex gap-7 flex-col shadow-2xl px-8 py-14 outline outline-1 outline-black/20 rounded-lg'>
+                    <h1 className='font-bold text-3xl'>SignIn Here</h1>
+                    <form onSubmit={signIn} className="gap-3 flex flex-col">
+                        <input
+                            type="email"
+                            placeholder="Email Address"
+                            name="email" value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="shadow-lg outline outline-1 outline-black/20 rounded-sm px-2 py-3"/>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="shadow-lg outline outline-1 outline-black/20 rounded-sm px-2 py-3"/>
+                        <input
+                            type="submit"
+                            value="submit"
+                            className='bg-themeColor shadow-lg text-white px-1 py-2'/>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
