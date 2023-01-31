ef7ed033ee3314bc5909812ca10bf00963c780ab
vaibhav-OP <vaibhavyash28@gmail.com>
2023-01-31 18:09:56 +0530

    added dropdown menu for chapters selection

---
+++ packages/client/app/comic/[id]/chapter/[chapterId]/[name]/ChaptersDropDown.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import Link from "next/link";
+import { useState } from "react";
+import { FaArrowDown } from "react-icons/fa";
+
+const ChaptersDropDown = ({ name, chapters, comicId, chapterId }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function handleDrop(e) {
+        e.preventDefault();
+
+        setIsOpen(bool => !bool)
+    }
+
+    const li = chapters.map((chapter, index) => {
+        return <li key={index}
+            className="py-2 px-3 hover:bg-[#e6e6e6] w-full">
+                <Link href={`/comic/${comicId}/chapter/${chapterId}/${chapter.name}`}>{chapter.name}</Link>
+            </li>
+    });
+
+    return <div className="relative w-32 sm:w-48 gap-1 flex flex-col">
+        <button className="h-fit min-h-[30px] px-3 flex justify-between items-center rounded-md bg-[#f0f1f2] font-bold cursor-pointer" onClick={handleDrop}>{name}<span><FaArrowDown /></span></button>
+        {isOpen && <ul className="bg-[#f0f1f2] flex rounded-md flex-col overflow-hidden">{li}</ul>}
+    </div>
+}
+
+export default ChaptersDropDown;
--- packages/client/app/comic/[id]/chapter/[chapterId]/[name]/Container.jsx
+++ packages/client/app/comic/[id]/chapter/[chapterId]/[name]/Container.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+
+import ChaptersDropDown from "./ChaptersDropDown";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 export default function ChapterContainer({ params, children, data, currentChapter, currentChapterIndex }) {
@@ -6,7 +8,7 @@ export default function ChapterContainer({ params, children, data, currentChapte
     const NextBtn = () => {
         if (currentChapterIndex < data.chapters.length-1) {
             return <Link href={`/comic/${params.id}/chapter/${params.chapterId}/${data.chapters[currentChapterIndex+1]?.name}`}
-                className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-gray-300 min-h-[30px] disabled:cursor-not-allowed cursor-pointer"><FaArrowRight /></Link>
+                className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-[#f0f1f2] hover:bg-[#e6e6e6] min-h-[30px] disabled:cursor-not-allowed cursor-pointer"><FaArrowRight /></Link>
         } else {
             return <div className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-gray-300 min-h-[30px] cursor-not-allowed opacity-60">
                 <FaArrowRight />
@@ -17,7 +19,7 @@ export default function ChapterContainer({ params, children, data, currentChapte
     const PreviousBtn = () => {
         if (currentChapterIndex > 0) {
             return  <Link href={`/comic/${params.id}/chapter/${params.chapterId}/${data.chapters[currentChapterIndex-1]?.name}`}
-                className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-gray-300 min-h-[30px] disabled:cursor-not-allowed cursor-pointer"><FaArrowLeft /></Link>
+                className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-[#f0f1f2] hover:bg-[#e6e6e6] min-h-[30px] disabled:cursor-not-allowed cursor-pointer"><FaArrowLeft /></Link>
         } else {
             return <div className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-gray-300 min-h-[30px] cursor-not-allowed opacity-60">
                 <FaArrowLeft />
@@ -28,13 +30,13 @@ export default function ChapterContainer({ params, children, data, currentChapte
     return <div className="flex flex-col gap-5">
         <div className="flex w-full gap-5 justify-center px-5 sm:px-0 ">
             <PreviousBtn />
-            <div className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-gray-300 min-h-[30px] font-bold">{currentChapter.name}</div>
+            <ChaptersDropDown name={currentChapter.name} chapters={data.chapters} comicId={params.id} chapterId={params.chapterId}/>
             <NextBtn />
         </div>
         {children}
         <div className="flex w-full gap-5 justify-center px-5 sm:px-0 ">
             <PreviousBtn />
-            <div className="w-32 sm:w-48 h-fit flex justify-center items-center rounded-md bg-gray-300 min-h-[30px] font-bold">{currentChapter.name}</div>
+            <ChaptersDropDown name={currentChapter.name} chapters={data.chapters} comicId={params.id} chapterId={params.chapterId}/>
             <NextBtn />
         </div>
     </div>
