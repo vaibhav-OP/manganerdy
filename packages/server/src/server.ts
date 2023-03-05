97cc77441519e814a0e4f92d5a8b7ed9a397aa3b
Vaibhav <vaibhavyash28@gmail.com>
2023-03-05 14:53:36 +0530

    Update server.ts

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'), {
+app.use(express.static(__dirname + '/public', {
     maxAge: 60 * 60 * 24
 
 }));
